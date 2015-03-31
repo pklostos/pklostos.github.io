21613a13e1dee8224577318151bce0c5bf056991
pklostos <peter.klostos@gmail.com>
2015-03-31 16:40:26 -0400

    changes

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var main = function() {
   $('.dropdown-toggle').click(function() {
     $('.dropdown-menu').toggle();
   });
-}
+  $('.jumbotron').click(function() {
+    $('.dropdown-menu').hide();
+  });
+};
+
 
 $(document).ready(main);
